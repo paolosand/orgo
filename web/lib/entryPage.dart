e92e107905c66797c51e269297fe688d00789773
Vitus Acabado <vitus.murdock16@gmail.com>
2021-03-23 19:35:10 +0800

    new file:   web/.flutter-plugins 	new file:   web/.flutter-plugins-dependencies 	new file:   web/.packages 	modified:   web/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java 	new file:   web/android/app/src/main/res/drawable-v21/background.png 	modified:   web/android/app/src/main/res/drawable-v21/launch_background.xml 	new file:   web/android/app/src/main/res/drawable/background.png 	modified:   web/android/app/src/main/res/drawable/launch_background.xml 	modified:   web/android/app/src/main/res/values-night/styles.xml 	modified:   web/android/app/src/main/res/values/styles.xml 	new file:   web/build/4f2a58af27abc49ecf55570043bbf938.cache.dill.track.dill 	new file:   web/build/51bcd9355023315736357aa3f0887d3a/gen_localizations.stamp 	new file:   web/build/flutter_assets/AssetManifest.json 	new file:   web/build/flutter_assets/FontManifest.json 	new file:   web/build/flutter_assets/NOTICES 	new file:   web/build/flutter_assets/fonts/MaterialIcons-Regular.otf 	new file:   web/build/flutter_assets/packages/cupertino_icons/assets/CupertinoIcons.ttf 	deleted:    web/integration_test/app_test.dart 	deleted:    web/integration_test/driver.dart 	new file:   web/ios/Runner/Assets.xcassets/LaunchBackground.imageset/Contents.json 	new file:   web/ios/Runner/Assets.xcassets/LaunchBackground.imageset/background.png 	modified:   web/ios/Runner/Assets.xcassets/LaunchImage.imageset/Contents.json 	modified:   web/ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage.png 	modified:   web/ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@2x.png 	modified:   web/ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@3x.png 	modified:   web/ios/Runner/Base.lproj/LaunchScreen.storyboard 	modified:   web/ios/Runner/GeneratedPluginRegistrant.m 	modified:   web/ios/Runner/Info.plist 	new file:   web/lib/entryPage.dart 	new file:   web/lib/generated_plugin_registrant.dart 	modified:   web/lib/loginPage.dart 	modified:   web/lib/main.dart 	new file:   web/lib/signupPage.dart 	modified:   web/pubspec.lock 	modified:   web/pubspec.yaml 	modified:   web/web/index.html 	new file:   web/web/splash/style.css

---
+++ web/lib/entryPage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:page_router/page_router.dart';
+
+class EntryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(0x58, 0x18, 0x45, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.only(bottom: 60.0),
+              child: Icon(
+                Icons.donut_large,
+                size: 100.0,
+                color: Color.fromRGBO(0xFF, 0xC3, 0x0F, 1),
+              ),
+            ),
+            Container(
+              height: 40.0,
+              width: 150.0,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(0xC7, 0x00, 0x39, 1),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  print("Login");
+                  PageRouter.of(context).pushNamed('/login');
+                },
+                child: Text(
+                  'LOG IN',
+                  style: TextStyle(color: Colors.white, fontSize: 15.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Container(
+              height: 40.0,
+              width: 150.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  print("Signup");
+                  PageRouter.of(context).pushNamed('/signup');
+                },
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(
+                      color: Color.fromRGBO(0xC7, 0x00, 0x39, 1),
+                      fontSize: 15.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
